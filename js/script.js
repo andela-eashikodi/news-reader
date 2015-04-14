949ec89a0e86a785a1df596ce0307fca120c73b2
ASHIKODI <ashikodiemeka@gmail.com>
2015-04-14 11:47:40 +0100

    filter feed

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 var App = angular.module('RSSFeedApp', []);
 
 App.controller("FeedCtrl", ['$scope','FeedService', function ($scope,Feed) {  
-  $scope.site = 'http://';
-    $scope.loadFeed=function(e){   
+  $scope.site = '';
+    $scope.loadFeed=function(e){ 
+    if($scope.site.substring(0,4)!=="http"){
+      console.log($scope.site.substring(0,4));
+      $scope.site = 'http://'+$scope.site;
+    }
+
       Feed.parseFeed($scope.site+"/feed").then(function(res){
         $scope.searchbox=angular.element(e.target).text();
         $scope.feeds=res.data.responseData.feed.entries;
       });
+    };
+
+    $scope.chkfeed = function(){
+
     };
 }]);
 
