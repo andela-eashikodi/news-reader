d3d7df80c19465c45b2442abf1f25717ba760d6d
ASHIKODI <ashikodiemeka@gmail.com>
2015-04-14 14:08:12 +0100

    alert on no feed

--- js/script.js
+++ js/script.js
@@ -2,33 +2,42 @@ var App = angular.module('RSSFeedApp', []);
 
 App.controller("FeedCtrl", ['$scope','FeedService', function ($scope,Feed) {  
   $scope.site = 'http://';
+  $scope.saved = [
+      {title: "first"},
+      {title: "second"}
+    ];
     $scope.loadFeed=function(e){
     $scope.loading = true; 
+    $scope.err = false;
     if($scope.site.substring(0,4)!=="http"){
-      console.log($scope.site.substring(0,4));
       $scope.site = 'http://'+$scope.site;
     }
+    
+    Feed.parseFeed($scope.site+"/feed").then(function(res){
+    $scope.feeds=res.data.responseData.feed.entries;
+    if($scope.feeds!==undefined){
+      $scope.loading = false;
+      // $scope.site = '';
+      $scope.err = "false";
+    }
+    });
 
-      Feed.parseFeed($scope.site+"/feed").then(function(res){
-        
-        $scope.site = '';
-        $scope.feeds=res.data.responseData.feed.entries;
-        if($scope.feeds!==undefined){
-          $scope.loading = false;
-        }
-        else if($scope.feeds===undefined){
-          alert("Couldn't locate feed for "+ $scope.site);
-        }
-      });
-
-      
+    if($scope.feeds===undefined){
+      $scope.loading = "false";
+      $scope.err = true;
+    }
     };
+
+    $scope.arr = function(){
+      $scope.saved.push({title:$scope.title});
+    };
+
 }]);
 
 App.factory('FeedService',['$http',function($http){
-    return {
-        parseFeed : function(url){
-            return $http.jsonp('//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
-        }
-    };
+  return {
+    parseFeed : function(url){
+      return $http.jsonp('//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
+    }
+  };
 }]);
