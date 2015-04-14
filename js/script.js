dc83197671c9b8b63c039f5f4a3ebe4c5f9641a2
ASHIKODI <ashikodiemeka@gmail.com>
2015-04-14 19:55:08 +0100

    feed check false

--- js/script.js
+++ js/script.js
@@ -5,29 +5,23 @@ App.controller("FeedCtrl", ['$scope','FeedService', function ($scope,Feed) {
   $scope.currentUrl='';
   $scope.saved = [
       
-    ];
+  ];
     
   $scope.loadFeed=function(e){
     $scope.currentUrl = $scope.site;
-    var urlPrefix="http://";
-    $scope.currentUrl = $scope.site.substr(7,$scope.site.length);
     console.log($scope.currentUrl);
     $scope.loading = true; 
     $scope.err = false;
     if($scope.site.substring(0,4)!=="http"){
       $scope.site = 'http://'+$scope.site;
     }
-    
+    $scope.currentUrl = $scope.site.substr(7,$scope.site.length);
+
     Feed.parseFeed($scope.site+"/feed").then(function(res){
     $scope.feeds=res.data.responseData.feed.entries;
-    if($scope.feeds!==undefined){
-      $scope.loading = false;
-      $scope.site = '';
-      $scope.err = false;
-    }
-    else {
-      $scope.err = false;
-    }
+    $scope.loading = false;
+    $scope.site = '';
+    $scope.err = false;
     });
 
     if($scope.feeds===undefined){
